9e52e197525f38a9a1577501932c17da36983488
joecass93 <joecass93@gmail.com>
2018-11-12 13:03:59 -0500

    trashed first django build to restart

--- nba_api_suite/nba_dashboard/api/resources.py
+++
@@ -1,8 +0,0 @@
-from tastypie.resources import ModelResource
-from api.models import Picks
-
-
-class PicksResource(ModelResource):
-    class Meta:
-        queryset = Picks.objects.all()
-        resource_name = 'picks'
